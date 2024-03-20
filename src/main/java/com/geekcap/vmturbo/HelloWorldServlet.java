c6a2c27fa7ab292ccae9d75912021d62999a5c4d
EnioAlcantara <enioha@gmail.com>
2024-03-20 18:53:12 -0300

    Update HelloWorldServlet.java
    implements Serializable

--- src/main/java/com/geekcap/vmturbo/HelloWorldServlet.java
+++ src/main/java/com/geekcap/vmturbo/HelloWorldServlet.java
@@ -3,8 +3,9 @@ package com.geekcap.vmturbo;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
+import java.io.Serializable;
 
-public class HelloWorldServlet extends HttpServlet 
+public class HelloWorldServlet extends HttpServlet implements Serializable
 {
 @Override
   public void service( HttpServletRequest req, HttpServletResponse res ) throws IOException {
